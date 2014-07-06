bef64e4d608ccf1052c9485e650c37785d7307cf
renjian93 <renjian93@gmail.com>
2014-07-06 15:12:36 +0800

    result FAIL

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,10 +1,20 @@
 package com.baldurtech.yolo.shame;
 public class FizzBuzzTest{
+	static Boolean testResult = true;
+	
 	public static void main(String[] args){
 		FizzBuzz app =new FizzBuzz();
-		System.out.println("1".equals(app.say(1)));
-		System.out.println("Fizz".equals(app.say(3)));
-		System.out.println("Buzz".equals(app.say(5)));
-		System.out.println("FizzBuzz".equals(app.say(15)));
-	} 
+
+		assertEquals("1","app.say(1)");
+		assertEquals("Fizz","app.say(3)");
+		assertEquals("Buzz","app.say(5)");
+		assertEquals("FizzBuzz","app.say(15)");
+		System.out.println("Test result "+(testResult ?"SUCCESS":"FAIL"));
+	}
+
+	public static void assertEquals(String expectedResult,String actualResult){
+		if(! expectedResult.equals(actualResult)){
+			testResult = false;
+		}
+	}
 } 
