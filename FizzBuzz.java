5145ddc44d44d77130010f803548f71150c45f3f
renjian93 <renjian93@gmail.com>
2014-07-06 18:03:07 +0800

    调用一次42后始终返回数字本身 测试用例从main方法中抽取出来

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,13 @@
 package com.baldurtech.yolo.shame;
 public class FizzBuzz{
+	Boolean ultimateAnswer = false;
 	public String say(Integer num){
 		if(num == 42) return "42";
+		if(ultimateAnswer){
+			return  String.valueOf(num);
+		}if(num == 42){
+			ultimateAnswer = true;
+		}
 		return num % 15 == 0 ?"FizzBuzz":num % 5 == 0 ?"Buzz":num % 3 == 0 ?"Fizz":String.valueOf(num); 	
 	}
 
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -3,16 +3,11 @@ public class FizzBuzzTest{
 	static Boolean testResult = true;
 	static Integer countSuccess = 0;
 	static Integer countFail = 0;
-	public static void main(String[] args){
-		FizzBuzz app =new FizzBuzz();
 
-		assertEquals("1",app.say(1));
-		assertEquals("Fizz",app.say(3));
-		assertEquals("Buzz",app.say(5));
-		assertEquals("FizzBuzz",app.say(15));
-		assertEquals("Fizz",app.say(42));
-		outputTestReport();
+	public static void main(String[] args){
+		runTest();
 	}
+
 	public static void assertEquals(String expectedResult,String actualResult){
 		if(! expectedResult.equals(actualResult)){
 			countFail ++;
@@ -22,8 +17,20 @@ public class FizzBuzzTest{
 			countSuccess ++;
 		}
 	}
+
 	public static void outputTestReport(){
 		System.out.println("Test result "+(testResult ?"SUCCESS":"FAIL"));
 		System.out.println("Test pass: "+countSuccess+" ,test fail: "+countFail);
+	
+	}
+	public static void runTest(){
+		FizzBuzz app =new FizzBuzz();
+
+		assertEquals("1",app.say(1));
+		assertEquals("Fizz",app.say(3));
+		assertEquals("Buzz",app.say(5));
+		assertEquals("FizzBuzz",app.say(15));
+		assertEquals("Fizz",app.say(42));
+		outputTestReport();
 	}
 } 
