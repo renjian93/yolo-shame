35c6839999c4d5f427ec3b75b974a2d40f6b66a4
renjian93 <renjian93@gmail.com>
2014-07-06 15:51:24 +0800

    统计测试通过数和失败数

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -1,7 +1,8 @@
 package com.baldurtech.yolo.shame;
 public class FizzBuzzTest{
 	static Boolean testResult = true;
-	
+	static Integer countSuccess = 0;
+	static Integer countFail = 0;
 	public static void main(String[] args){
 		FizzBuzz app =new FizzBuzz();
 
@@ -10,13 +11,19 @@ public class FizzBuzzTest{
 		assertEquals("Buzz",app.say(5));
 		assertEquals("FizzBuzz",app.say(15));
 		assertEquals("Fizz",app.say(42));
-		System.out.println("Test result "+(testResult ?"SUCCESS":"FAIL"));
+		outputTestReport();
 	}
-
 	public static void assertEquals(String expectedResult,String actualResult){
 		if(! expectedResult.equals(actualResult)){
+			countFail ++;
 			System.out.println("Expected "+expectedResult+"but result "+actualResult);
 			testResult = false;
+		}else{
+			countSuccess ++;
 		}
+	}
+	public static void outputTestReport(){
+		System.out.println("Test result "+(testResult ?"SUCCESS":"FAIL"));
+		System.out.println("Test pass: "+countSuccess+" ,test fail: "+countFail);
 	}
 } 
