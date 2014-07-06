7b1db49bf13c7f48521cf13674363d654d7daf12
renjian93 <renjian93@gmail.com>
2014-07-06 15:21:46 +0800

    result FAIL

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -5,10 +5,10 @@ public class FizzBuzzTest{
 	public static void main(String[] args){
 		FizzBuzz app =new FizzBuzz();
 
-		assertEquals("1","app.say(1)");
-		assertEquals("Fizz","app.say(3)");
-		assertEquals("Buzz","app.say(5)");
-		assertEquals("FizzBuzz","app.say(15)");
+		assertEquals("1",app.say(1));
+		assertEquals("Fizz",app.say(3));
+		assertEquals("Buzz",app.say(5));
+		assertEquals("FizzBuzz",app.say(15));
 		System.out.println("Test result "+(testResult ?"SUCCESS":"FAIL"));
 	}
 
